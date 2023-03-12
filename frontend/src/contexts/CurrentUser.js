4e472741a82ebe6df8a20ec327db2eec1bdd08b9
Andrew <andrewcrink@gmail.com>
2023-03-12 13:52:24 -0400

    added roles and different functionality for each role

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,10 +3,11 @@ import { createContext, useState, useEffect } from "react";
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
-    useEffect (() => {
+
+    useEffect(() => {
 
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/authentication/profile', {
